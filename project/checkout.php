33afa172ccb160cdd77e168af05b058f3012f082
sheng10086@gmail.com <sheng10086@gmail.com>
2017-12-04 12:35:45 -0500

    new tables created

--- project/checkout.php
+++ project/checkout.php
@@ -4,17 +4,7 @@
 	$dbServername="localhost";
 	$dbUsername="root";
 	$dbPassword="12345";
-	$dbName="ecommence"; 
-
-if (isset($_POST['keepsearch'])){
-	echo "you want keep searching??";
-	header( "refresh:2;url=searchresults.php" );
-	exit();
-}
-else if (isset($_POST['checkout'])){
-	header( "refresh:2;url=checkout.php" );
-	exit();	
-	
+	$dbName="ecommence";	
 	$conn=mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 	$itemsInShoppingCart=$_SESSION['productsAdded'];	
 	for($i=0;$i<count($itemsInShoppingCart);$i++){
@@ -30,14 +20,10 @@ else if (isset($_POST['checkout'])){
 			echo "<br>";
 			echo $itemsInShoppingCart[$i][4];
 			echo "<br>";
-	$updateQuantity=$itemsInShoppingCart[$i][3]-1;
-	echo "<br>";
-	$sql= "UPDATE inventory SET TotalQuantity =  ".$updateQuantity." WHERE SellerId = " .$itemsInShoppingCart[$i][0]. " AND ItemId=". $itemsInShoppingCart[$i][1]. ";";
-	$result=mysqli_query($conn,$sql);
+	}
 	
-	}	
-}
+	
+	
+	include_once 'footer.php';
 
-
-	include_once 'footer.php'
 ?>
